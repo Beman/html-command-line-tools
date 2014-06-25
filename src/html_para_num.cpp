9b7468066a3afa486e8b5de768070f2efe8e7184
Beman <bdawes@acm.org>
2014-06-25 08:21:26 -0400

    Initial commit.

--- src/html_para_num.cpp
+++ src/html_para_num.cpp
@@ -28,12 +28,106 @@
 //--------------------------------------------------------------------------------------// 
 
 #include <iostream>
-#include <fstream>
 #include <string>
+#include <cstring>  // for strchr
 #include <boost/detail/lightweight_main.hpp>
 
+using std::cin;
 using std::cout;
 using std::cerr;
 using std::endl;
 using std::string;
 using std::getline;
+
+namespace
+{
+  bool header(const string& line)
+  {
+    string::size_type pos = line.find("<h");
+    return pos != string::npos && (pos + 2) < line.size() && isdigit(line[pos+2]);
+  }
+
+  string::size_type find_start(const string& line, string::size_type start_pos)
+    // returns pos for insertion start
+  {
+    string::size_type pos = line.find("<p>", start_pos);
+    if (pos != string::npos)
+      pos += 2;
+
+    string::size_type pos2 = line.find("<p ", start_pos);
+    if (pos2 < pos)
+      pos += 2;
+
+    pos2 = line.find("<pre>", start_pos);
+    if (pos2 < pos)
+      pos += 4;
+
+    pos2 = line.find("<pre ", start_pos);
+    if (pos2 < pos)
+      pos += 4;
+
+    return pos;
+  }
+
+}  // unnamed namespace
+
+//--------------------------------------------------------------------------------------// 
+
+int cpp_main(int argc, char* argv[])
+{
+  if (argc > 1)
+  {
+    cout << "Usage: html_para_num <stdin >stdout\n"
+      "  Input lines copied to output, adding paragraph numbers to <p> and\n"
+      "  <pre> elements.\n"
+      "  Input lines containing \"generate-paragraph-numbers=false\" or\n"
+      "  \"generate-paragraph-numbers=true\" cause indicated action. Default true.\n"
+      "  Warning: assumes heading, <p>, and <pre> markup is lower case.\n"
+      "  Warning: assumes heading elements (<h1>, etc) never appear on same line\n"
+      "  as <p> or <pre> elements."
+      ;
+    return 1;
+  }
+
+  string line;
+  string::size_type pos;
+  bool generate = true;
+  int para_num = 0;
+
+  while (getline(cin, line))  // input each line
+  {
+    if (line.find("generate-paragraph-numbers=false") != string::npos)
+    {
+      generate = false;
+    }
+    else if (line.find("generate-paragraph-numbers=true") != string::npos)
+    {
+      generate = true;
+    }
+    else if (header(line))
+    {
+      para_num = 0;
+    }
+    else if (generate)
+    {
+      // if there are already para_num's on line, erase them
+      while ((pos = line.find(" para_num=\"")) != string::npos)
+      {
+        line.erase(pos, 11);
+        while (std::strchr("0123456789\"", line[pos]))
+          line.erase(pos, 1);
+      }
+
+      for (pos = 0; (pos = find_start(line, pos)) != string::npos; pos += 13)
+      {
+        line.insert(pos, string(" para_num=\"") + std::to_string(++para_num) + '"' );
+      }
+    }
+
+    // output the line
+    std::cout << line << '\n';
+  }
+
+
+  return 0;
+}
