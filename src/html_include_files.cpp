775f99baecf973213f6d4288ae7f7c4a96670af5
Beman <bdawes@acm.org>
2016-01-18 19:48:57 -0500

    Code complete and working fairly well. Issue: <pre>...</pre> spacing for code snippets - we do not want leading or trailing \n.

--- src/html_include_files.cpp
+++ src/html_include_files.cpp
@@ -19,12 +19,15 @@ using namespace std;
 
 namespace
 {
-  const string start_str("<!-- include file \"");
+  const string start_str("<!-- include \"");
   const string start_snippet_str("snippet=");
   const string label_chars(
     "_1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz");
-  const string start_end_str("-->");
-  const string end_str("<!-- end include file -->");
+  const string start_end_str(" -->");
+  const string end_str("<!-- end include -->");
+
+  const string snip_begin_str("<!-- snippet=");
+  const string snip_end_str("<!-- end snippet -->");
 
   const string::size_type min_sz =
     start_str.size() + start_end_str.size() + end_str.size();
@@ -35,21 +38,21 @@ namespace
 
   string get_snippet_name(string::size_type& pos)
   {
-    // if a snippet name is found, pos is set to the first non-space after the name
+    // if "snippet=name" is found, pos is set to the first non-space after the name
     // otherwise, pos is unchanged.
     string name;
     string::size_type cur = pos;
     while (cur < buf.size() && buf[cur] == ' ') { ++cur; }
     if (cur == buf.size())
     {
-      cout << "\nError - end of input file before end of \"<!-- include file\" marker";
+      cout << "\nError - end of input file before end of \"<!-- include\" marker";
       exit(1);
     }
     string::size_type end(buf.find(start_snippet_str, cur));
     if (end == cur) // "snippet=" found
     {
       cur += start_snippet_str.size();  // cur now at curition after the equal sign
-      if ((end = buf.find_first_not_of(label_chars, cur)) == string::ncur)
+      if ((end = buf.find_first_not_of(label_chars, cur)) == string::npos)
       {
         cout << "\nError - end of input file while processing \"snippet=name\"";
         exit(1);
@@ -98,21 +101,19 @@ int cpp_main( int argc, char* argv[] )
   {
     cout << "Usage: html_include_files [--htmlize] input-file output-file\n";
     cout <<
-      "Scans input-file for text in the form:\n"
-      "       <!-- include file \"include-path\" [snippet=name]-->\n"
-      "       ...\n"
-      "       <!-- end include file -->\n"
-      "   and replaces ... with the contents of include-path.\n"
-      "   If optional \"snippet=name\" is present, only the named snippet will be\n"
-      "   included; otherwise the entire file will be included.\n"
-      "   Snippets begin with the line after a line that contains:\n"
-      "       <!-- snippet=name -->\n"
-      "   and end with the line before a line that contains:\n"
-      "       <!-- snippet end -->\n"
-      "   Note: For including C/C++ files, the snippet begin and end markers can be\n"
-      "   placed in C/C++ comments."
-      "   Option --htmlize causes '\"', '&', '<', and '<' in the include file to be\n"
-      "   replaced by \"&quot;\", \"&amp;\", \"&lt;\", and \"&gt;\", respectively."
+      "  Scans input-file for text in the form:\n"
+      "   <!-- include \"include-path\" [snippet=name]-->...<!-- end include -->\n"
+      "  and replaces ... with the contents of include-path.\n"
+      "  If optional \"snippet=name\" is present, only the named snippet will be\n"
+      "  included; otherwise the entire file will be included.\n"
+      "  Snippets begin with the line after a line that contains:\n"
+      "      <!-- snippet=name -->\n"
+      "  and end with the line before a line that contains:\n"
+      "      <!-- end snippet -->\n"
+      "  Note: For including C/C++ files, the snippet begin and end markers can be\n"
+      "  placed in C/C++ comments."
+      "  Option --htmlize causes '\"', '&', '<', and '<' in the include file to be\n"
+      "  replaced by \"&quot;\", \"&amp;\", \"&lt;\", and \"&gt;\", respectively."
       ;
     return 1;
   }
@@ -151,24 +152,53 @@ int cpp_main( int argc, char* argv[] )
       string include_file(buf.substr(pos, end - pos));
       cout << "include file \"" << include_file << "\"\n";
 
-      pos = end;
-      string snippet_name(get_snippet_name(pos));  // updates pos, empty() if not found
+      pos = end+1;
+      string snippet_name(get_snippet_name(pos));  // updates pos if snippet=name found
+      if (buf.find(start_end_str, pos) == pos) // account for start_end_str
+        pos += start_end_str.size();
+      else
+      {
+        cout << "Error: " << start_end_str << " after \"" << include_file
+          << "\" not found where expected.";
+        exit(1);
+      }
 
       if ((end = buf.find(end_str, pos)) != string::npos)
       {
         ifstream finc(include_file);
         if (!finc)
         {
-          cout << "file not found" << endl;
+          cout << "include file not found" << endl;
           ++error_count;
           continue;
         }
         string rep;
-        string::size_type pos_num_sign;
         getline(finc, rep, '\0'); // read entire file
 
-        if (start_at_number_sign && (pos_num_sign = rep.find("#")) != string::npos)
-          rep.erase(0, pos_num_sign);
+        if (!snippet_name.empty())
+        {
+          string::size_type snip_pos;
+          string begin_marker(snip_begin_str + snippet_name + " -->");
+          if ((snip_pos = rep.find(begin_marker)) == string::npos)
+          {
+            cout << "Error: could not find " << begin_marker << " in " << include_file
+              << endl;
+            exit(1);
+          }
+          snip_pos += begin_marker.size();
+          while (snip_pos < rep.size()
+            && (buf[snip_pos] == '\r' || buf[snip_pos] == '\n')) {
+            ++snip_pos;
+          }
+          rep.erase(0, snip_pos);
+          if ((snip_pos = rep.find(snip_end_str)) != string::npos)
+          {
+            while (snip_pos != 0
+              && (buf[snip_pos] == '\r' || buf[snip_pos] == '\n')) { --snip_pos; }
+            rep.erase(snip_pos);
+          }
+        }
+
         replace_ascii_chars_with_html_names(rep);
         if (!rep.empty() && rep[rep.size() - 1] == '\n')
           rep.erase(rep.size() - 1);  // erase unwanted trailing newline
@@ -176,6 +206,11 @@ int cpp_main( int argc, char* argv[] )
         buf.insert(pos, rep);
         // do not further adjust pos since included file may itself contains includes
         ++file_count;
+      }
+      else
+      {
+        cout << "Warning: \"-->\" not found at end of marker for file "
+          << include_file << endl;
       }
     }
   }
